07dd38a509635ad7702e2742aa96dcab8b251f8c
YgorRupp <ygor.rupp@hotmail.com>
2023-05-17 11:13:13 -0300

    finish portifolio

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Ygor Rupp",
+  githubUser: "YgorRupp",
+  linkedinUser: "ygorrupp",
+  whatsappNumber: "+5571999943626",
+  emailUser: "ygor.rupp@hotmail.com",
+  instagramUser: "ygorrupp",
   facebookUser: "",
 };
